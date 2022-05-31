8f549a15165d9687d4ffc6292b17fdce4b3d1ae5
jiangyi <189363638@qq.com>
2022-05-31 11:04:26 +0800

    【功能添加】：webview页面添加

---
+++ pages/webview/webview.vue
@@ -0,0 +1,16 @@
+<template>
+	<web-view :src="path"></web-view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return { path:'https://blog.csdn.net/qq_35430000/article/details/119356158?utm_term=uniapp%20env%E6%96%87%E4%BB%B6&utm_medium=distribute.pc_aggpage_search_result.none-task-blog-2~all~sobaiduweb~default-0-119356158-null-null&spm=3001.4430' }
+		},
+		methods: {}
+	}
+</script>
+
+<style>
+
+</style>
